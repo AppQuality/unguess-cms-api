93fd6f7571f9c5465718612d711ef527817f3814
Marco Bonomo <marco.bonomo@unguess.io>
2022-05-19 12:42:43 +0200

    set preview config

--- config/api.js
+++ config/api.js
@@ -4,4 +4,23 @@ module.exports = {
     maxLimit: 100,
     withCount: true,
   },
+  'preview-button': {
+    enabled: true,
+    config: {
+      contentTypes: [
+        {
+          uid: 'api::service.service',
+          targetField: 'slug',
+          draft: {
+            query: {
+              type: 'service',
+            },
+          },
+          published: {
+            basePath: 'services',
+          },
+        }
+      ],
+    },
+  },
 };
